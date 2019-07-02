aff4df0891a18dffcc4cce85b1709de87032a1b7
Mirona108 <mironaws@yahoo.com>
2019-07-02 18:14:05 +0300

    Modifications

--- src/main/java/com/example/recipeproject/controllers/IndexController.java
+++ src/main/java/com/example/recipeproject/controllers/IndexController.java
@@ -5,11 +5,13 @@ import com.example.recipeproject.model.UnitOfMeasure;
 import com.example.recipeproject.repositories.CategoryRepository;
 import com.example.recipeproject.repositories.UnitOfMeasureRepository;
 import com.example.recipeproject.services.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.Optional;
 
+@Slf4j
 @Controller
 public class IndexController {
 
